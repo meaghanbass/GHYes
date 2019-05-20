1826db59e496340fa5e41f1503bcce1475693b90
Meaghan Bass <meaghan.bass@gmail.com>
2019-05-20 12:32:53 -0700

    nav work

--- yesapp/src/app/app.component.ts
+++ yesapp/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Youth Education Society';
+  navLink1 = 'About Us';
+  navLink2 = 'Our Services';
+  navLink3 = 'Our Economy';
+  navLink4 = '#';
+  navLink5 = 'Research Center';
+  navLink6 = 'News & Events';
 }
